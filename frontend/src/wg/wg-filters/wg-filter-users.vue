8f0e496609e1297a70a049d3b128bdd05ca31baf
mex1986fox <mex1986fox@mail.ru>
2019-04-11 16:54:33 +0700

    111

--- frontend/src/wg/wg-filters/wg-filter-users.vue
+++ frontend/src/wg/wg-filters/wg-filter-users.vue
@@ -2,10 +2,10 @@
   <wg-filter :show="show" @onHide="isHide">
     <template slot="header">Фильтр пользователей</template>
     <template slot="content">
-      <ui-ef-text value="Текст" caption="Текст" class="ui-ef-text_white"></ui-ef-text>
-      <ui-ef-content caption="sdfsdf" class="ui-ef-content_white" :autoresize="150"></ui-ef-content>
-      <wg-multi-location class="wg-multi-location_white"/>
-      <wg-select-location class="wg-select-location_white"/>
+      <ui-ef-text caption="Логин" class="ui-ef-text_white"></ui-ef-text>
+      <ui-ef-text caption="Имя" class="ui-ef-text_white"></ui-ef-text>
+      <ui-ef-text caption="Фамилия" class="ui-ef-text_white"></ui-ef-text>
+      <wg-multi-location caption="Места проживания" class="wg-multi-location_white"/>
     </template>
     <template slot="buttons">
       <input class="ui-button ui-button_float_white" type="button" value="Применить">
